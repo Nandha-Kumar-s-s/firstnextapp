a8a2607e8893e06e93f521635035f8c19e34dd74
Nandha-Kumar-s-s <nandhakumar834427@gmail.com>
2024-07-22 18:28:17 +0530

    Private folder

---
+++ app/_lib/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Lib() {
+  return (
+    <div>You cannot access the private folder</div>
+  )
+}
+
+export default Lib
--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,7 +1,7 @@
 const NotFound = () => {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>Not Found</h1>
+            <h1>Page Not Found</h1>
         </main>
     );
 }
