5045a4f0e89549019559e8ee4075f0dd49f4232a
Nandha-Kumar-s-s <nandhakumar834427@gmail.com>
2024-07-22 18:24:48 +0530

    File colocation

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,8 @@
 
 "use client";
 import { useRouter } from "next/navigation";
+import BarChart from "../components/dashboard/BarChart";
+import LineChart from "../components/dashboard/LineChart";
 
 
 
@@ -15,6 +17,8 @@ export default function Dashboard() {
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={back}>Back</button>
         <h1>Dashboard Page</h1>
+        <BarChart />
+        <LineChart />
       </main>
     );
   }
