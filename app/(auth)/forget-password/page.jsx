3a5f52f49fd3d508b08bfbb6f2aebb6229c77cf9
Nandha-Kumar-s-s <nandhakumar834427@gmail.com>
2024-07-22 18:38:01 +0530

    Route Groups

---
+++ app/(auth)/forget-password/page.jsx
@@ -0,0 +1,9 @@
+const ForgetPassword = () =>{
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Forget Password</h1>
+        </main>
+    )
+}
+
+export default ForgetPassword
---
+++ app/(auth)/register/page.jsx
@@ -0,0 +1,10 @@
+const Register = () => {
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Register</h1>
+        </main>
+    )
+}
+
+export default Register
